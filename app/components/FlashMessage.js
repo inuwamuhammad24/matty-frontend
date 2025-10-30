5cf9339d652ad3f1bf7a0cfee5020a9747e21785
inuwamuhammad24 <inuwamuhammad24@gmail.com>
2025-10-30 23:18:32 +0100

    Added a maintenance message

--- app/components/FlashMessage.js
+++ app/components/FlashMessage.js
@@ -3,17 +3,18 @@ import { AnimatePresence, easeInOut, motion } from "framer-motion"
 
 function FlashMessage(props) {
   useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       props.setState(draft => {
         draft.isFlashVisible = false
       })
-    }, 3000)
+    }, 6000)
+    return () => clearTimeout(timer)
   }, [props.isFlashVisible])
   return (
     <AnimatePresence>
       {props.isFlashVisible && (
-        <motion.div initial={{ x: 300, opacity: 0 }} animate={{ x: 0, opacity: 1 }} exit={{ x: 300, opacity: 0 }} transition={{ duration: 0.33, ease: easeInOut }} className="fixed top-8 right-4 bg-[#12181F] rounded-md border border-[#6b6b6b80] px-8 py-4 text-[#f7e4e4] z-5 flex items-center gap-2">
-          <i className="bx  bxs-info-circle text-lg text-[#d44950]"></i>
+        <motion.div initial={{ x: 300, opacity: 0 }} animate={{ x: 0, opacity: 1 }} exit={{ x: 300, opacity: 0 }} transition={{ duration: 0.33, ease: easeInOut }} className="fixed top-8 right-4 rounded-md border px-4 py-4 z-5 flex items-center gap-2 max-w-3/4 lg:max-w-1/3 bg-gradient-to-r from-rose-500 to-red-600 text-white border-none">
+          <i className="bx  bxs-info-circle text-lg bg-white/20 text-white rounded-full"></i>
           <p className="">{props.message}</p>
         </motion.div>
       )}
